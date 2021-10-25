40a4657681bf3c0ec1f158689aae7bda0f928991
leedoopal <39582981+leedoopal@users.noreply.github.com>
2021-10-25 20:42:18 +0900

    add 114 sum of two integers

---
+++ 114-SumOfTwoIntegers/SumOfTwoIntegers.js
@@ -0,0 +1,23 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-medium/114/others/822/
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+const getSum = function(a, b) {
+  let start = a;
+  let sum;
+
+  while (b !== 0) {
+    start = a ^ b;
+    sum = (a & b) << 1;
+    console.log(start, (a & b), sum);
+    a = start;
+    b = sum;
+  }
+
+  return a;
+};
+
+console.log(getSum(2, 3));
