4441644486e049de7c425453a8bb7fe4fe87c379
leedoopal <1233wsh@gmail.com>
2021-12-18 15:06:39 +0900

    추가 795 Permutations

---
+++ 795-Permutations/Permutations.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+const permute = function(nums) {
+    const result = [];
+
+    function loop (arr) {
+        if (arr.length === nums.length){
+            result.push(arr);
+            return;
+        }
+
+        for (let i = 0; i < nums.length; i++) {
+            if (!arr.includes(nums[i])) {
+                loop([...arr, nums[i]])
+            }
+        }
+    }
+
+    loop([])
+
+    return result;
+};
+
+console.log(permute([1,2,3]));
