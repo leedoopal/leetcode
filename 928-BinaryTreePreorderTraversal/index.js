75e75e83fd4757ecbc2a2372bee26d3ff95ddb48
leedoopal <1233wsh@gmail.com>
2022-11-07 22:51:50 +0900

    928 - binary tree preorder traversal

---
+++ 928-BinaryTreePreorderTraversal/index.js
@@ -0,0 +1,38 @@
+class TreeNode {
+  constructor(item) {
+    this.val = item;
+    this.left = this.right = null;
+  }
+}
+
+const result = [];
+let root;
+
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+const preorderTraversal = function(root) {
+  if (!root) return [];
+
+  const arr = [];
+  arr.push(root);
+
+  while(arr.length > 0) {
+    const node = arr.pop();
+    result.push(node.val);
+
+    if (node.right !== null) arr.push(node.right)
+    if (node.left !== null) arr.push(node.left)
+  }
+
+  return result;
+};
+
+root = new TreeNode(1);
+root.left = new TreeNode(2);
+root.right = new TreeNode(3);
+root.left.left = new TreeNode(4);
+root.left.right = new TreeNode(5);
+
+console.log(preorderTraversal(root));
