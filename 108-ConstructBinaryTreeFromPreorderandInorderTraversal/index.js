a4c414bae7e68684ef54fd4807127d7ee9d85e46
leedoopal <1233wsh@gmail.com>
2022-11-14 23:04:09 +0900

    108 - construct binary tree preorder and inorder traversal

---
+++ 108-ConstructBinaryTreeFromPreorderandInorderTraversal/index.js
@@ -0,0 +1,46 @@
+class TreeNode {
+  constructor(item) {
+    this.val = item;
+    this.left = this.right = null;
+  }
+}
+
+let root;
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+let preorderIndex = 0;
+const buildTree = function (preorder, inorder) {
+  const inorderMap = new Map();
+  for (let i = 0; i < inorder.length; i++) {
+    inorderMap.set(inorder[i], i)
+  }
+
+  return arrayToTree(preorder, inorderMap, 0, inorder.length - 1)
+};
+
+const arrayToTree = (preorder, inorderMap, left, right) => {
+  if (left > right) return null;
+
+  const rootValue = preorder[preorderIndex++];
+  const inorderRootValue = inorderMap.get(rootValue);
+
+  const root = new TreeNode(rootValue);
+  root.left = arrayToTree(preorder, inorderMap, left, inorderRootValue - 1)
+  root.right = arrayToTree(preorder, inorderMap, inorderRootValue + 1, right)
+
+  return root;
+}
+
+console.log(buildTree([-1], [-1]));
