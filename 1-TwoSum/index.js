f5943b40d165359854d76773192d4500ece300e7
leedoopal <1233wsh@gmail.com>
2023-01-12 23:14:43 +0900

    1 - two sum

---
+++ 1-TwoSum/index.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = function(nums, target) {
+  const map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const diff = target - nums[i];
+    if (diff in map) return [map[diff], i];
+    map[nums[i]] = i;
+  }
+};
+
+console.log(twoSum([2,7,11,15], 9))
