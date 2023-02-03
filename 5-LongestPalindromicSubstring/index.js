77239f16fdb57ddf55b352c106c4999484eff486
leedoopal <1233wsh@gmail.com>
2023-02-03 22:08:23 +0900

    5 - 주석 추가

--- 5-LongestPalindromicSubstring/index.js
+++ 5-LongestPalindromicSubstring/index.js
@@ -8,11 +8,14 @@ var longestPalindrome = function (s) {
   let answer = '';
 
   for (let i = 0; i < s.length; i++) {
+    // j = 0: left = right 일 때
+    // j = 1: left 다음 right가 left = right라면 서로 다른 때까지 확장됨.
+    // -> 가장 긴 하위 문자열을 얻음
     for (let j = 0; j < 2; j++) {
       let left = i;
-      let right = left + 1;
+      let right = left + j;
 
-      while (s[left] === s[right]) {
+      while (s[left] && s[left] === s[right]) {
         left--;
         right++;
       }
