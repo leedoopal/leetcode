e23bbef80a0e81a99bcae6bc8abad87e63ef48b0
우성화 <1233wsh@gmail.com>
2023-10-29 20:39:53 +0900

    45 다시풀기

--- 45-JumpGameII/index.js
+++ 45-JumpGameII/index.js
@@ -3,23 +3,23 @@
  * @return {number}
  */
 var jump = function(nums) {
-  // 현재 점프할 수 있는 최대 인덱스
-  let current = -1;
-  // 다음 점프할 수 있는 최대 인덱스
-  let next = 0;
-  // 점프 횟수 카운트
-  let result = 0;
+  // 점프 수
+  let jump = 0;
+  // 이전에 가능했던 점프 범위
+  let prev = 0;
+  // 현재 점프 가능한 범위
+  let curr = 0;
 
-  for (let i = 0; next < nums.length - 1; i++) {
-    // 점프를 해야하는 상황
-    if (i > current) {
-      result++;
-      current = next;
+  for (let i = 0; i < nums.length; i++) {
+    curr = Math.max(curr, nums[i] + i);
+
+    if (prev === i) {
+      prev = curr;
+      jump++;
     }
-    next = Math.max(next, nums[i] + i)
   }
 
-  return result;
+  return jump;
 };
 
 console.log(jump([2, 3, 0, 1, 4]));
